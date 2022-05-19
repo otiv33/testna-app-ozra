77d4112eeaba767b0e76d7c18b8d1bb0fa489a7f
Vito Abeln <vito.abeln@gmail.com>
2022-05-19 15:44:30 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import SerbusCounter from './components/SerbusCounter/SerbusCounter';
+import SeznamZelja from './components/SeznamZelja/SeznamZelja';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <SeznamZelja naslov="Seznam želja" navodilo="Kaj si želimo? 🤷"></SeznamZelja>
+      </div>
     </div>
   );
 }
